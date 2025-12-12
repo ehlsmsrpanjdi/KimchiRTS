a15e7a4137805e9010a95202f977eb0fe9933c07
ehlsmsrpanjdi <usm9401@gmail.com>
2025-12-12 20:34:11 +0900

    dotween설치 및 ui만듦

---
+++ Assets/Script/UI/BuildingSystem/BuildingSystemToggleUI.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingSystemToggleUI : MonoBehaviour
+{
+    public Image toggleImg;
+
+    private void Reset()
+    {
+        toggleImg = GetComponent<Image>();
+    }
+}
